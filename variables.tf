c58115aa58a7d8fb7c7f7b043e01292f44f57d8b
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2024-07-17 12:26:12 +0530

    Create variables.tf-commit1

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  description = "The AWS region to deploy in"
+  default     = "us-east-1"
+}
+
+variable "ami" {
+  description = "The AMI ID for the EC2 instance"
+  default     = "ami-0c55b159cbfafe1f0" // Example AMI ID, replace with your own
+}
+
+variable "instance_type" {
+  description = "The instance type to use"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "The name tag for the EC2 instance"
+  default     = "example-instance"
+}
