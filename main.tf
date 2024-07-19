848dc1a1d1688fce5e1842490ac657e864f50f9a
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2024-07-19 13:00:28 +0530

    Update main.tf-commit6

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  backend "s3" {
+    bucket = "jenkins-ec2-bucket1"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = var.ami
   instance_type = var.instance_type
